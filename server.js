06fe8140e8a49eb5583548b696d938176094ccaa
Promit Panja <promitpanja627@gmail.com>
2020-11-06 12:36:54 +0530

    a new wave of theories

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const articleRouter = require('./routes/articles')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+
+app.set('view engine', 'ejs')
+
+
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+app.use('/public', express.static('public'))
+
+
+
+app.get('/', async (req, res) => {
+    const article = await Article.find().sort({
+        createdAt: 'desc'
+    })
+    res.render('articles/index', { article: article })
+})
+
+
+
+app.use('/articles', articleRouter)
+app.listen(4000)
